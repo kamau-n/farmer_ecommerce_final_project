5a8b7a5801cc37141e327424ce223668c024cba9
KamauHarrison <100772289+kamau-n@users.noreply.github.com>
2025-06-26 19:35:09 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "farmersapp",
-      script: "index.js", // or your main entry file
+      script: "/dist/index.js", // or your main entry file
       cwd: "/var/www/farmersapp",
       watch: false,
       env: {
